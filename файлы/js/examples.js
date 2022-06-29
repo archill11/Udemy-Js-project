ddf0c31efb2e50fd34b8761517f538e727a89a75
Bogdan <example@gmail.com>
2022-06-29 12:33:11 +0300

    first test commit

---
+++ файлы/js/examples.js
@@ -0,0 +1,9 @@
+const storeName = 'Zara'
+let storeDescription = {
+    budget: 10000,
+    employees: ['Pavel', 'Bogdan', 'Kotlin'],
+    products: {tv: 1000, ps: 2000},
+    open: true
+}
+
+console.log(Y)
---
+++ файлы/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// console.log(11*112)
+// console.log('петя')
+// console.log('petr')
+/////////////////////////////////////////////
+//  const result = confirm('Are You Here?')
+//  console.log(result)
+//  alert(result)
+///////////////////////////////////////////////
+ const answers = []
+ answers[0] = prompt('wats your name ', '')
+ answers[1] = prompt('wats your surname ', '')
+ answers[2] = prompt('how old are you ', '')
+
+ document.write(answers + 'truten')
